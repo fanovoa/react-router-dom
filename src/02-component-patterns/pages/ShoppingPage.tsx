cbf1c8c38a0cce0036b79058722e6091759d8b9a
fanovoa <fabian93novoa@gmail.com>
2023-08-24 11:40:42 -0500

    separacion de componentes

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,4 @@
-import { ProductCard } from "../components/ProductCard"
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
 
 const product ={
     id: '1',
@@ -19,7 +19,17 @@ export const ShoppingPage = () => {
             flexWrap:'wrap'
         }}>
 
-        <ProductCard  product={product}/>
+            <ProductCard  product={ product }>
+                <ProductCard.Image />
+                <ProductCard.Title  />
+                <ProductCard.Buttons  />
+            </ProductCard>
+
+            <ProductCard  product={ product }>
+                <ProductImage />
+                <ProductTitle title={'Cafe'} />
+                <ProductButtons />
+            </ProductCard>
         </div>
     </div>
   )
